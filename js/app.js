20ddd22962703f4e35e31313b91c204a6fe2fdbe
git config --global user.name "Link1965 <git config --global user.email nicci.hydak@gmail.com>
2016-02-25 18:13:25 -0500

    Addition of NaN

--- js/app.js
+++ js/app.js
@@ -7,68 +7,63 @@ $(document).ready(function(){
   var guess;
   var counter = 0
 
-    $("#guessButton").click(function() {
-    	var guess = $("#userGuess").val();
-  		$("#userGuess").val('');
-    	  allGuesses.push(guess);
-     	if (guess != '') {
-     		 $("#guessList").append("<li>" + guess + "</li>");
-     		 counter++;
-     	}
-     	
-	$("#count").text(counter);
+  $("#guessButton").click(function() {
+    var guess = $("#userGuess").val();
+  	$("#userGuess").val('');
+    	allGuesses.push(guess);
+    if (guess != '') {
+      $("#guessList").append("<li>" + guess + "</li>");
+    }
+    if (isNaN(guess)) {
+    alert("Whoops! That's not a number!");
+    return false;
+    }
 
-     console.log('Guess = ' + guess);
+  counter++;
+
+  $("#count").text(counter);
+
+  console.log('Guess = ' + guess);
     
-    diff = Math.abs(guess - randomNumber);
+  diff = Math.abs(guess - randomNumber);
 
-      if (diff >= 80) {
-         message = "You're ice cold!"
-      } 
-      else if ((diff >= 60) && (diff < 80)) {
-        message = "Very cold!"
-      }
-      else if ((diff >= 40) && (diff < 60)) {
-      	message = "Cold!"
-      }
-
-      else if ((diff >= 20) && (diff < 40)) {
-      	message = "Still pretty chilly!"
-      }
-
-      else if ((diff >= 11) && (diff < 20)) {
-    	 message = "You're warm!"
-      }
-      else if ((diff >= 1) && (diff < 11)) {
-      	message = "You're hot!"
-      }
-
-	  else if (guess == randomNumber) {
-        message = "You got it! Click '+	NEW GAME'!"
-      }
+  if (diff >= 80) {
+    message = "You're ice cold!"
+  } 
+  else if ((diff >= 60) && (diff < 80)) {
+    message = "Very cold!"
+  }
+  else if ((diff >= 40) && (diff < 60)) {
+    message = "Cold!"
+  }
+  else if ((diff >= 20) && (diff < 40)) {
+    message = "Still pretty chilly!"
+  }
+  else if ((diff >= 11) && (diff < 20)) {
+    message = "You're warm!"
+  }
+  else if ((diff >= 1) && (diff < 11)) {
+    message = "You're hot!"
+  }
+  else if (guess == randomNumber) {
+    message = "You got it! Click '+	NEW GAME'!"
+  }
 
 	$("#feedback").text(message);
+   return false;
 
-    return false;
+});
 
-   });
+/*--- Display information modal box ---*/
+$(".what").click(function(){
+  $(".overlay").fadeIn(1000);
+});
 
-
-    
-
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-
-  	});
+/*--- Hide information modal box ---*/
+$("a.close").click(function(){
+  $(".overlay").fadeOut(1000);
+});
   	
-
 });
  
 $("a.new").click(function() {
